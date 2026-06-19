184feddb47736f5af1825640a8c9a12246c45f71
Stking19 <okolistephen43@gmail.com>
2026-06-19 19:01:22 +0100

    added animation

--- src/pages/Homeroute.jsx
+++ src/pages/Homeroute.jsx
@@ -1,12 +1,24 @@
 import Header from "../components/navbar/Header"
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from "motion/react";
 import Footer from '../components/footer/Footer'
 
 const Homeroute = () => {
+  const location = useLocation();
+
   return (
     <>
     <Header/>
-    <Outlet/>
+    <AnimatePresence mode="wait">
+      <motion.main
+        key={location.pathname}
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.65, ease: "easeOut" }}
+      >
+        <Outlet />
+      </motion.main>
+    </AnimatePresence>
     <Footer />
     </>
   )
